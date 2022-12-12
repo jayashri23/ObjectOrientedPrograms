1b763ac280b57783ab2889f60d8f1dccc48b3b03
jayashri23 <jsjankar23@gmail.com>
2022-12-13 05:24:44 +0530

    Added to shuffle card between 4 players and print that cards by using…

---
+++ src/com/DeckOfCards/DeckOfCard.java
@@ -0,0 +1,28 @@
+package com.DeckOfCards;
+
+public class DeckOfCard {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Play cards game");
+        String[] suit = {"Clubs", "Diamonds", "Hearts", "Spades"};
+        String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10",
+                "Jack", "Queen", "King", "Ace"};
+        int n = suit.length * rank.length;   //// initialize deck
+        String[] deck = new String[n];
+        for (int i = 0; i < rank.length; i++) {
+            for (int j = 0; j < suit.length; j++) {
+                deck[suit.length*i + j] = rank[i] + " of " + suit[j];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {       // shuffle deck
+            int r = i + (int) (Math.random() * (n-i));
+            String temp = deck[r];
+            deck[r] = deck[i];
+            deck[i] = temp;
+        }
+        for (int i = 0; i < 4; i++) {       // print shuffled deck
+            for (int j = 0; j < 9; j++)
+                System.out.println(deck[i]);
+        }
+    }
+}
